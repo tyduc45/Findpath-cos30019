4c36f74d448eb347709dff63b58653cb7978bc6e
JiangZhongyu <lexyukino@outlook.com>
2025-04-02 23:55:20 +1100

    modify the output methods

--- algorithm_jzy/gbfs.py
+++ algorithm_jzy/gbfs.py
@@ -5,46 +5,42 @@ class GBFS:
     def __init__(self, graph, start, ends):
         self.graph = graph
         self.start = start
-        self.ends = set(ends)  # Convert end nodes to a set for faster lookup
+        self.ends = set(ends)
         self.heap = MinHeap()
-        self.came_from = {}  # Store the path
-        self.end = None  # Mark the destination node once found
+        self.came_from = {}
+        self.nodes_created = 0
+        self.result_path = None
+        self.end = None  # 将在 search 中设置为实际到达的目标点
 
     def gbfs_calculate(self):
-        # Initialize the heap with the start node and an initial cost of 0
         self.heap.push((0, self.start))
         self.came_from[self.start] = None
 
         while len(self.heap) > 0:
             current_cost, current_node = self.heap.pop()
+            self.nodes_created += 1  # 访问节点数增加
 
-            # If the current node is one of the target nodes
             if current_node in self.ends:
                 self.end = current_node
                 return True
 
-            # For each neighbor not yet visited
             for neighbor, cost in self.graph.get_neighbors(current_node).items():
                 if neighbor not in self.came_from:
                     self.came_from[neighbor] = current_node
+                    self.heap.push((cost, neighbor))  # 这里只用 cost 做启发排序
 
-                    # Estimate cost using a simple heuristic to the closest end node
-                    heuristic_cost = min(
-                        [self.graph.get_edge_weight(neighbor, end) or float('inf') for end in self.ends]
-                    )
-                    total_cost = current_cost + cost
-                    self.heap.push((heuristic_cost + total_cost, neighbor))
-
-        # If no path was found
         return False
 
     def get_result(self):
-        # Reconstruct the path from end to start using came_from
+        if self.end is None:
+            return None, None, self.nodes_created
+
         path = []
         step = self.end
         while step is not None:
             path.append(step)
             step = self.came_from.get(step)
+        path.reverse()
 
-        path.reverse()  # Return path from start to end
-        return path
+        self.result_path = path
+        return self.end, self.nodes_created, path
